0ab437ced9d1ac00712bb885eed039fadd1c96e5
Henrique <henriquelopes0611@gmail.com>
2025-05-02 12:11:54 -0300

    Solve exercise 1.3

--- part1/jsbasics/arrays.js
+++ part1/jsbasics/arrays.js
@@ -15,9 +15,22 @@
  * immutable data structures in React.
  */
 
-const t = [1, -1, 3];
-
+const t = [1, 2, 3];
 const t2 = t.concat(5);
-
 console.log(t);
 console.log(t2);
+
+/* map() method */
+const m1 = t.map((value) => value * 2);
+console.log(m1);
+
+const m2 = t.map((value) => "<li>" + value + "</li>");
+console.log(m2);
+
+/* destructuring assignment */
+t.push(4);
+t.push(5);
+
+const [first, second, ...rest] = t;
+console.log(first, second);
+console.log(rest);
